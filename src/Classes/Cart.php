ea4c3dcb79a6308c21b271e2cd3c0ac072dc5cd2
Boubacar Gueye <bgueye@gmail.com>
2021-07-19 00:50:38 +0200

    gestion panier

---
+++ src/Classes/Cart.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Classes;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class Cart 
+{
+    private $session;
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session){
+        $this->session = $session;
+        $this->em = $entityManager;
+    }
+
+    public function getFull()
+    {
+        $cartComplete = [];
+        
+        if ($this->get()){
+        
+            foreach ($this->get() as $id =>$quantity){
+                $product_object = $this->em->getRepository(Product::class)->find($id);
+
+                if(!$product_object){
+                    $this->delete($id);
+                    continue;
+                }
+
+                $cartComplete[] = [
+                    'product' => $product_object,
+                    'quantity' => $quantity
+
+                ];
+            }
+        }
+
+        return $cartComplete;
+    }
+
+    public function add($id)
+    {
+
+        $cart = $this->session->get('cart', []);
+
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        }else{
+            $cart[$id] = 1;
+        }
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function get()
+    {
+        return $this->session->get('cart');
+    }
+
+    public function delete($id)
+    {
+
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function remove()
+    {
+        return $this->session->remove('cart');
+    }
+
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if($cart[$id] > 1){
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+
+        return $this->session->set('cart', $cart);
+
+    }
+
+}
