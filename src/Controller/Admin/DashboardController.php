701a022ad168ab7d919a19af13d0f34fce28752e
Boubacar Gueye <bgueye@gmail.com>
2021-07-28 18:21:28 +0200

    ajout gestion header backoffice

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Entity\Carrier;
 use App\Entity\Product;
 use App\Entity\Category;
 use App\Controller\Admin\OrderCrudController;
+use App\Entity\Header;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -41,6 +42,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
         yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
+        yield MenuItem::linkToCrud('Headers', 'fas fa-desktop', Header::class);
         
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,9 +23,10 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $products = $this->em->getRepository(Product::class)->findByIsBest(1);
-        
+        $headers = $this->em->getRepository(Header::class)->findAll();
         return $this->render('home/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'headers' => $headers
         ]);
     }
 }
